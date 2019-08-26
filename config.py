9532e82da8a4af19ed16c69058977009e64ea7cb
Prosper Atukwatse <pa5@williams.edu>
2019-08-26 19:17:59 +0300

    Fix indentation issue

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ class Config:
 	SECRET_KEY = os.getenv('SECRET_KEY')
 
 class ProdConfig(Config):
-    DEBUG = False
+	DEBUG = False
 
 class CeleryConfig:
 	broker_url = os.getenv('REDIS_URL')
 
 class AWSConfig:
 	aws_access_key_id = os.getenv('aws_access_key_id')
-    aws_secret_access_key = os.getenv('aws_secret_access_key')
+	aws_secret_access_key = os.getenv('aws_secret_access_key')
